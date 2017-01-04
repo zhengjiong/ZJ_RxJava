39b2ee58ba687c5055f3054b056acf8828f38272
zhengjiong <zhengjiong2006@gmail.com>
2017-01-04 09:06:56 +0800

    add switchMap

---
+++ RxJavaTest/src/main/java/com/example/operator/Example34SwitchMap.java
@@ -0,0 +1,23 @@
+package com.example.operator;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func1;
+
+/**
+ * Title: Example34SwitchMap
+ * Description:
+ * Copyright:Copyright(c)2016
+ * Company: 博智维讯信息技术有限公司
+ * CreateTime:17/1/4  08:45
+ *
+ * @author 郑炯
+ * @version 1.0
+ */
+public class Example34SwitchMap {
+
+    //http://www.jianshu.com/p/33c548bce571
+}
